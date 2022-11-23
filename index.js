6c481caa0e8fcf0fd43b7245c919508f03517d54
Caliph Dev <admin@caliph.my.id>
2022-11-23 21:15:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(STATIC_ROOT));
 app.use(async (req, res, next) => {
   res.locals.titleweb = "Tikly";
   res.locals.req = req;
@@ -189,7 +190,6 @@ app.get("/robots.txt", (req, res) => {
       "/sitemap.xml"
   );
 });
-app.use(express.static(STATIC_ROOT));
 
 app.use((req, res) => res.status(404).render("404"))
 
